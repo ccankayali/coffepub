8a29dea662b9380eb6c1844af2d1b465df8818f9
ccankayali <volkand135@gmail.com>
2024-06-01 00:11:33 +0300

    Site

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,8 +1,11 @@
+import Navbar from '@/components/Navbar/Navbar'
 import React from 'react'
 
 function page() {
   return (
-    <div className='bg-primary'>page</div>
+    <div>
+      <Navbar />
+    </div>
   )
 }
 
