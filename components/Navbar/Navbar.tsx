436d8bf35895610acb43e0e0c74703c4469b6cdf
ccankayali <volkand135@gmail.com>
2024-06-03 20:29:31 +0300

    Navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -2,37 +2,40 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { Button } from '../ui/button'
+import OrderButton from '../Button/OrderButton'
+import { FaCoffee } from 'react-icons/fa'
 
 function Navbar() {
   return (
-    <div>
-        <div className='flex justify-center p-3 bg-amber-950 text-white'>
-            <div className='flex items-center space-x-2 mt-1'>
-              <Image src="/images/logo/2.png" alt="logo" className='pl-1 mb-2' width={35} height={5} />
-              <Link href="/" className="text-2xl font-pacifiko">CoffePub</Link>
-            </div>
-
-            <div className='flex-1 flex justify-center space-x-4'>
-              <Button variant='navbar' size='sm' >
-                <Link href="/" className="text-lg">Home</Link>
-              </Button>
-              <Button variant='navbar' size='sm'>
-                <Link href="/about" className="text-lg">About</Link>
-              </Button>
-              <Button variant='navbar' size='sm'>
-                <Link href="/contact" className="text-lg">Contact</Link>    
-              </Button>
-            </div>
-
-            <div className='flex justify-end space-x-4'>
-              <Button variant='navbar' size='sm'>
-                <Link href="/login" className="text-lg">Login</Link>
-              </Button>
-              <Button variant='navbar' size='sm'>
-                <Link href="/signup" className="text-lg">Sign Up</Link>
-              </Button>
-            </div>
+    <div className='bg-amber-950 text-white p-3'>
+      <div className='flex justify-between items-center'>
+        <div className='flex items-center space-x-2 lg:text-3xl md:text-2xl' data-aos="fade">
+          <Image src="/images/logo/2.png" alt="logo" width={35} height={35} />
+          <Link href="/" className="text-2xl font-pacifiko">CoffePub</Link>
         </div>
+
+        <div className='space-x-4 hidden lg:flex' data-aos="fade-down">
+          <Link href="/">
+            <Button variant='navbar' size='sm'>
+              <span className="text-lg">Home</span>
+            </Button>
+          </Link>
+          <Link href="/about">
+            <Button variant='navbar' size='sm'>
+              <span className="text-lg">About</span>
+            </Button>
+          </Link>
+          <Link href="/contact">
+            <Button variant='navbar' size='sm'>
+              <span className="text-lg">Contact</span>
+            </Button>
+          </Link>
+        </div>
+
+        <div  data-aos-once="true" data-aos-delay="500">
+          <OrderButton icon={FaCoffee} label='Order' />
+        </div>
+      </div>
     </div>
   )
 }
--- components/Navbar/Navbaritem.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Navbaritem() {
-  return (
-    <div>Navbaritem</div>
-  )
-}
-
-export default Navbaritem
